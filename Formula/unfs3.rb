c39c4daa4043e69fd1b59b873ff78420d174bd73
Derek Bender <nofeardjb@gmail.com>
2017-09-26 20:15:13 -0500

    i don't know why this is different

--- Formula/unfs3.rb
+++ Formula/unfs3.rb
@@ -2,7 +2,7 @@ class Unfs3 < Formula
   desc "User-space NFSv3 server"
   homepage "http://unfs3.github.io"
   url "https://github.com/unfs3/unfs3/archive/unfs3-0.9.22.tar.gz"
-  sha256 "394a5df491f4e8caeacfed0fec86690f7772badb79be986191f0d598a0d38995"
+  sha256 "9708d2012b072d0cfdc32559a07344cd8d17f44e3118874e0c2471b9877032d7"
 
   head do
     url "https://github.com/unfs3/unfs3.git"
